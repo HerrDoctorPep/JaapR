1e95a935641779841723e7939aa3a40bef9d4bfb
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2019-04-27 14:57:10 +0200

    Clean scraped data - completed Add postal code geo locations - completed Add visualization through Google static map API - completed

--- JaapScraper.r
+++ JaapScraper.r
@@ -75,6 +75,9 @@ for (p in 1:maxPage){
 
 write_csv(huizen_html, path="huizen_20190424.csv", na = "NA", append = FALSE, col_names = TRUE)
 
+
+
+
 # make numbers; for analysis purposes
 
 huizen_data <- huizen_html %>%
@@ -82,6 +85,7 @@ huizen_data <- huizen_html %>%
   mutate(m2 = as.integer(substring(trimws(m2), 1,nchar(m2)-3))) %>%
   mutate(kamers = as.integer(substring(trimws(kamers), 1,nchar(kamers)-7))) %>%
   mutate(postcode4 = as.integer(substring(postcode,1,4))) %>%
+  mutate(postcode6 = str_replace_all(substring(postcode,1,7)," ","")) %>%
   mutate(prijspm2 = prijs / m2)
 
 # exclude some very big or expensive houses
@@ -117,13 +121,70 @@ ggplot() +
   ggtitle("Prijs per m2 naar postcode4") +
   geom_point(data=huizen_clean,aes(postcode4,prijspm2,col=postcode4))
 
-huizen_bypc <- huizen_clean %>%
-  group_by('postcode4') %>%
-  summarise(prijs_avg = mean(prijs),
-            prijspm2_avg = mean(prijspm2))
-
 
 # ggplot() +
 #   ggtitle("Avg prijs per m2 naar postcode4") +
 #   geom_col(data=huizen_bypc,aes(postcode4,prijspm2_avg,col=postcode4))
 
+#
+# Add postal code data
+#
+
+# Load postal codes
+# Note: there are some doubling rows to remove
+
+library('readxl')
+postcode_data <- read_xlsx("C:/Users/micro/HiDrive/RawData/postcodetabel/postcodetabel.xlsx")
+postcode_data <- postcode_data %>%
+  select(PostCode, Latitude, Longitude) %>%
+  group_by(PostCode) %>%
+  summarise(Latitude = mean(Latitude),
+            Longitude = mean(Longitude))
+
+# Create table by postal code
+
+huizen_bypc <- huizen_clean %>%
+  group_by(postcode6) %>%
+  summarise(m2_avg = mean(m2),
+            prijs_avg = mean(prijs),
+            kamers_avg = mean(kamers)
+  ) %>%
+  mutate(prijspm2_avg = prijs_avg / m2_avg,
+         m2pkamer = m2_avg / kamers_avg)
+
+huizen_forplot <- huizen_bypc %>%
+  left_join(postcode_data,by = c("postcode6" = "PostCode"))
+
+# Try mapping stuff
+
+library(ggmap)
+library(maps)
+library(mapdata)
+
+ggplot() + 
+  geom_polygon(data = map_data("world", region="netherlands"), aes(x=long, y = lat, group = group),colour="grey",fill="lightgrey") + 
+  coord_fixed(1.3) +
+  geom_point(data = huizen_forplot, aes(x = Longitude, y = Latitude,colour=log(prijs_avg)), size = 1) +
+  labs(title = "Huizenprijzen in Rotterdam", x="Longitude", y="Lattitude")
+
+
+mean(huizen_forplot$Longitude[!is.na(huizen_forplot$Longitude)])
+mean(huizen_forplot$Latitude[!is.na(huizen_forplot$Latitude)])
+
+(map_Roffa13 <- get_googlemap(c(4.48,51.915),key="AIzaSyDFp1dCbSx2aQGFakv5Uup71wximjGaDg8", zoom=13))
+
+ggmap(map_Roffa13) +
+  geom_point(data = huizen_forplot, aes(x = Longitude, y = Latitude,colour=log(prijs_avg)), size = 2) +
+  labs(title = "Huizenprijzen in Rotterdam", x="Longitude", y="Lattitude")
+
+ggmap(map_Roffa13) +
+  geom_point(data = huizen_forplot, aes(x = Longitude, y = Latitude,colour=prijspm2_avg), size = 2) +
+  labs(title = "Vierkantemeterprijzen in Rotterdam", x="Longitude", y="Lattitude")
+
+ggmap(map_Roffa13) +
+  geom_point(data = subset(huizen_forplot,m2_avg>100), aes(x = Longitude, y = Latitude,colour=prijspm2_avg), size = 2) +
+  labs(title = "Vierkantemeterprijzen in Rotterdam (m²>100)", x="Longitude", y="Lattitude")
+
+ggmap(map_Roffa13) +
+  geom_point(data = huizen_forplot, aes(x = Longitude, y = Latitude,colour=m2_avg), size = 2) +
+  labs(title = "Woninggrootte in Rotterdam", x="Longitude", y="Lattitude")
